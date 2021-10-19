45252b14d3dc28fc50e413c7fbe89d9a62b5f716
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-19 15:48:07 +0300

    Добавьте файлы проекта.

---
+++ Time/Time.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void time(int h, int m)
+{
+    cout << "Time is " << h << ":" << m;
+}
+
+int main()
+{
+    int hour, minute;
+    cout << "Enter the number of hours: ";
+    cin >> hour;
+    cout << "Enter the number of minutes: ";
+    cin >> minute;
+    time(hour,minute);
+}
